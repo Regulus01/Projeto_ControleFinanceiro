0cd3de509c113faaf0edc553582a0fe3c2c89483
josePc <j.sousa.jr@hotmail.com>
2023-06-08 19:56:56 -0300

    feat(Gastos): Insercao de coluna tipoDoGasto

---
+++ Infra/Infra.Authentication/Migrations/20230608224558_update_gastos_tipo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Authentication.Migrations
+{
+    /// <inheritdoc />
+    public partial class updategastostipo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Tipo",
+                schema: "Autenticacao",
+                table: "Gasto",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                schema: "Autenticacao",
+                table: "Gasto");
+        }
+    }
+}
